15142559899425ed063e67e315641ffe026fad84
vietnv <nguyenvanvietbn1998@gmail.com>
2025-03-24 11:12:23 +0700

    update 5

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,8 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Ospreyfx Official Site',
-  description: 'Ospreyfx Official Site. We needs to review the security of your connection before proceeding Ospreyfx',
+  title: 'Media Mister Official Site',
+  description: 'Media Mister Official Site. We needs to review the security of your connection before proceeding Media Mister',
 }
 
 export default function RootLayout({ children }) {
