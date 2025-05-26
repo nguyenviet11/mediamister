43c0bf27111686d51618255622b5d2b066e63b69
vietnv-mmo <nguyenvanvietbn1998@gmail.com>
2025-05-26 14:15:59 +0700

    uppps

--- src/app/mediamister-connecting/page.js
+++ src/app/mediamister-connecting/page.js
@@ -16,7 +16,7 @@ export default function page() {
     <div>
       <main className={styles.main}>
         <h3 className={styles.text}>
-        Media Mister Official Site{" "}
+        Media Mister Official Site{""}
           <span
             style={{ transform: "translate(0px)", display: "inline-block" }}
           >
